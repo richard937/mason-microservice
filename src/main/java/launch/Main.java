8fc47125592129f5d3c9a993b84179da10f902f3
Deepak Mishra <d3ep4k@gmail.com>
2019-10-20 21:13:45 +0530

    Working

--- src/main/java/launch/Main.java
+++ src/main/java/launch/Main.java
@@ -13,6 +13,7 @@ import javax.sql.DataSource;
 import java.io.File;
 
 public class Main {
+
     public static void main(String[] args) throws Exception {
 
         String webappDirLocation = "src/main/webapp/";
