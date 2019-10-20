b2184324b5ee5a214c62336a1a76cedcd153fdff
Deepak Mishra <d3ep4k@gmail.com>
2019-10-20 07:17:17 +0530

    Added embedded tomat and jasper

--- src/main/java/launch/Main.java
+++ src/main/java/launch/Main.java
@@ -1,12 +1,13 @@
 package launch;
 
-import java.io.File;
-
+import org.apache.catalina.LifecycleException;
 import org.apache.catalina.WebResourceRoot;
 import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.catalina.webresources.DirResourceSet;
 import org.apache.catalina.webresources.StandardRoot;
+
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -23,7 +24,9 @@ public class Main {
 
         tomcat.setPort(Integer.valueOf(webPort));
 
-        StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+
+        StandardContext ctx = (StandardContext) tomcat.addWebapp("/rest", new File(webappDirLocation).getAbsolutePath());
+        ctx.setDefaultContextXml("/META-INF/context.xml");
         System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
 
         // Declare an alternative location for your "WEB-INF/classes" dir
@@ -34,7 +37,14 @@ public class Main {
                 additionWebInfClasses.getAbsolutePath(), "/"));
         ctx.setResources(resources);
 
-        tomcat.start();
+        tomcat.enableNaming();
+        ctx.setDefaultWebXml(new File("/WEB-INF/web.xml").getAbsolutePath());
+        tomcat.getConnector();
+        try {
+            tomcat.start();
+        } catch (LifecycleException e) {
+            e.printStackTrace();
+        }
         tomcat.getServer().await();
     }
 }
