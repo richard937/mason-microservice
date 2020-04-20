4ab497a6b97d6faea6b80c12a0224276fdca1878
Deepak Mishra <d3ep4k@gmail.com>
2020-04-20 16:22:19 +0530

    added execute example

---
+++ src/main/java/com/example/RequestHandler.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import com.example.entity.Customer;
+import com.metamug.entity.Request;
+import com.metamug.entity.Response;
+import com.metamug.exec.RequestProcessable;
+import java.util.Map;
+import javax.sql.DataSource;
+
+
+/**
+ *
+ * @author anishhirlekar
+ */
+public class RequestHandler implements RequestProcessable {
+
+    @Override
+    public Response process(Request request, DataSource ds, Map<String, Object> args) throws Exception {
+
+
+        Customer customer = new Customer();
+        customer.setName("John Doe.");
+        customer.setId(8);
+        customer.setContact("+1 943 322 4292", "john.doe@gmail.com");
+
+        Response response = new Response();
+        // set your model object as payload here
+        response.setPayload(customer);
+        return response;
+    }
+
+}
